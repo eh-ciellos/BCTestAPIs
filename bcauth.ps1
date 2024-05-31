feb8c7ad042d06fbbcb06cdf45066510c2c85c8d
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2024-05-31 12:19:45 +0200

    Update bcauth.ps1

--- bcauth.ps1
+++ bcauth.ps1
@@ -12,6 +12,14 @@ function Get-PlainText() {
         [Runtime.InteropServices.Marshal]::FreeBSTR($bstr);
     }
 }
+function Parse-JWTtoken([string]$token) {
+    if ($token.Contains(".") -and $token.StartsWith("eyJ")) {
+        $tokenPayload = $token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+        while ($tokenPayload.Length % 4) { $tokenPayload += "=" }
+        return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($tokenPayload)) | ConvertFrom-Json
+    }
+    throw "Invalid token"
+}
 
 function New-BcAuthContext {
     Param(
